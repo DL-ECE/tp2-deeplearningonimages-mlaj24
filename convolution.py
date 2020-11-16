1d8cfbcce39d81e352df2d8d8193cdbd1e5ed856
mlaj24 <36448042+mlaj24@users.noreply.github.com>
2020-11-16 09:49:22 +0100

    TP2 finished (Bonus section not answered)

--- convolution.py
+++ convolution.py
@@ -767,34 +767,34 @@ Use some already trained CNN to segment YOUR image.
 In the cell below your can load a image to the notebook and use the given network to have the segmentation mask and plot it.
 """
 
-if __name__ == "__main__" :
+#if __name__ == "__main__" :
     
     # TODO HERE: Upload an image to the notebook in the navigation bar on the left
     # `File` `Load File`and load an image to the notebook. 
     
-    filename = "" 
-    # Loading a already trained network in pytorch 
-    model = torch.hub.load('pytorch/vision:v0.6.0', 'deeplabv3_resnet101', pretrained=True)
-    model.eval()
+#    filename = "" 
+#    # Loading a already trained network in pytorch 
+#    model = torch.hub.load('pytorch/vision:v0.6.0', 'deeplabv3_resnet101', pretrained=True)
+#    model.eval()
 
-    from PIL import Image
-    from torchvision import transforms
+#    from PIL import Image
+#    from torchvision import transforms
 
-    input_image = Image.open(filename)
-    preprocess = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+#    input_image = Image.open(filename)
+#    preprocess = transforms.Compose([
+#        transforms.ToTensor(),
+#        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#    ])
 
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+#    input_tensor = preprocess(input_image)
+#    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
     # move the input and model to GPU for speed if available
-    if torch.cuda.is_available():
-        input_batch = input_batch.to('cuda')
-        model.to('cuda')
+#    if torch.cuda.is_available():
+#        input_batch = input_batch.to('cuda')
+#        model.to('cuda')
 
-    with torch.no_grad():
-        output = model(input_batch)['out'][0]
-    output_predictions = output.argmax(0)
+#    with torch.no_grad():
+#        output = model(input_batch)['out'][0]
+#    output_predictions = output.argmax(0)
 
